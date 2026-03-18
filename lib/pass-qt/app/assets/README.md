3b76a952766e81bd92634edd607105605c5174c0
John Doe <johndoe@example.com>
2026-03-18 23:27:38 +0800

    feat: Update app icon

---
+++ lib/pass-qt/app/assets/README.md
@@ -0,0 +1,5 @@
+# Assets
+
+## License
+
+- [app.svg](./app.svg) come from [vinceliuice/Colloid-icon-theme](https://github.com/vinceliuice/Colloid-icon-theme)
--- lib/pass-qt/config/initializers/qt6.rb
+++ lib/pass-qt/config/initializers/qt6.rb
@@ -1,3 +1,8 @@
 QApplication.set_organization_name("souk4711")
 QApplication.set_application_name("PassQt")
-QApplication.set_window_icon(QIcon.from_theme("pass-qt"))
+
+root_path = File.expand_path("../..", __dir__)
+QDir.add_search_path("assets", File.join(root_path, "app/assets"))
+
+icon = QIcon.new("assets:/app.svg")
+QApplication.set_window_icon(icon)
